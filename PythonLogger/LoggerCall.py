abc9fc00af9da7141460aa31c2f57276f7c0a246
Muthukumar <53684606+kumarmuthu@users.noreply.github.com>
2024-07-19 17:53:37 +0530

    Update LoggerCall.py

--- PythonLogger/LoggerCall.py
+++ PythonLogger/LoggerCall.py
@@ -3,15 +3,15 @@ from LoggerLib import LoggerLib
 __version__ = "2019.10.20.01"
 __author__ = "Muthukumar Subramanian"
 
-# You can refer this link also
+# You can refer to this link also
 # https://stackoverflow.com/questions/6386698/how-to-write-to-a-file-using-the-logging-python-module
 
 
 # Calling **LoggerLib** function
 file_name = 'muthu'
 lib_obj = LoggerLib()
-log_obj = lib_obj.Logger(file_name)
-log_obj.info("I am from loggercall class".format())
+log_obj = lib_obj.logger_func(file_name)
+log_obj.info("I am from logger call class".format())
 log_obj.critical("CRIC".format())
 log_obj.error("ERR".format())
 log_obj.warning("WARN".format())
